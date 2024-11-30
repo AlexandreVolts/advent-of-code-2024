b5997da529c6bcf517025a0176748f7690719077
Alexandre C <beejayvolts@gmail.com>
2024-12-01 00:03:47 +0100

    feat(ex0)

--- lib/advent_of_code2024.ex
+++ lib/advent_of_code2024.ex
@@ -2,14 +2,18 @@ defmodule AdventOfCode2024 do
   use Application
   def read_file(file_name) do
       {:ok, file} = File.open(file_name, [:read])
-      content = IO.read(file, :line)
+      content = IO.read(file, :eof)
       File.close(file)
 
-      content
+      String.split(content, "\n")
   end
 
+  def main() do
+    AdventOfCode2024.read_file("assets/0.txt") |> Exercise_0.ex1() |> IO.puts()
+    AdventOfCode2024.read_file("assets/0.txt") |> Exercise_0.ex2() |> IO.puts()
+  end
   def start(_type, _args) do
-    IO.puts(AdventOfCode2024.read_file("assets/1.txt"))
+    main()
     Supervisor.start_link([], strategy: :one_for_one)
   end
 end
