3988a114a6f56ac412e630a32afc03a63f044e6b
Alexandre C <beejayvolts@gmail.com>
2024-12-01 10:54:58 +0100

    feat(ex01)

---
+++ lib/Exercise0.ex
@@ -0,0 +1,23 @@
+defmodule Exercise0 do
+  @spec get_integer_from_first_and_last_char(charlist()) :: integer()
+  def get_integer_from_first_and_last_char(str) do
+    str = Regex.replace(~r/[[:alpha:]]/, str, "")
+    (String.first(str) <> String.last(str)) |> String.to_integer()
+  end
+
+  @spec extract_integer_from_str(charlist()) :: charlist()
+  def extract_integer_from_str(str) do
+    numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    numbers |> Enum.with_index() |> Enum.reduce(str, fn {x, index}, acc -> acc |> String.replace(x, x <> Integer.to_string(index + 1) <> x) end)
+  end
+
+  @spec ex1(list()) :: integer()
+  def ex1(lines) do
+    lines |> Enum.reduce(0, fn x, acc -> acc + get_integer_from_first_and_last_char(x) end)
+  end
+
+  @spec ex2(list()) :: integer()
+  def ex2(lines) do
+    lines |> Enum.map(&extract_integer_from_str/1) |> ex1()
+  end
+end
---
+++ lib/Exercise1.ex
@@ -0,0 +1,27 @@
+defmodule Exercise1 do
+  @spec get_integer_from_line(binary(), integer()) :: integer()
+  def get_integer_from_line(line, index) do
+    line |> String.split(" ", trim: true) |> Enum.at(index) |> String.to_integer()
+  end
+
+  @spec get_sorted_integer_list(list(charlist()), integer()) :: list(integer())
+  def get_sorted_integer_list(lines, index) do
+    lines |> Enum.map(fn line -> get_integer_from_line(line, index) end) |> Enum.sort()
+  end
+
+  @spec ex1(list(charlist())) :: integer()
+  def ex1(lines) do
+    l1 = lines |> get_sorted_integer_list(0)
+    l2 = lines |> get_sorted_integer_list(1)
+
+    l1 |> Enum.with_index() |> Enum.reduce(0, fn {x, index}, acc -> acc + abs(x - (l2 |> Enum.at(index))) end)
+  end
+
+  @spec ex2(list(charlist())) :: integer()
+  def ex2(lines) do
+    l1 = lines |> get_sorted_integer_list(0)
+    l2 = lines |> get_sorted_integer_list(1)
+
+    l1 |> Enum.reduce(0, fn x, acc -> acc + x * (l2 |> Enum.count(fn y -> x == y end)) end)
+  end
+end
