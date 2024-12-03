5d31a8865b3dfc9a675a2bd4c7a526f27d17b6b0
Alexandre C <beejayvolts@gmail.com>
2024-12-03 14:28:59 +0100

    feat(ex03)

---
+++ lib/Exercice3.ex
@@ -0,0 +1,35 @@
+defmodule Exercise3 do
+  @spec strmul_to_integer(charlist()) :: integer()
+  defp strmul_to_integer(multiplication) do
+    values = Regex.scan(~r/[0-9]{1,3}/, multiplication)
+            |> Enum.map(fn array -> hd(array) |> String.to_integer() end)
+    hd(values) * (values |> tl() |> hd())
+  end
+
+  @spec get_mul(charlist()) :: integer()
+  defp get_mul(line) do
+    Regex.scan(~r/mul\(([0-9]{1,3}),([0-9]{1,3})\)/, line)
+    |> Enum.map(fn array -> hd(array) |> strmul_to_integer() end)
+    |> Enum.reduce(fn x, acc -> acc + x end)
+  end
+
+  @spec remove_disable_parts(charlist()) :: charlist()
+  defp remove_disable_parts(line) do
+    disabled = "don't()"
+    enabled = "do()"
+    chunks = line |> String.split(disabled, trim: true)
+
+    [hd(chunks)] ++ (tl(chunks) |> Enum.map(fn str -> str |> String.split(enabled) |> tl() |> Enum.join("") end))
+    |> Enum.join("")
+  end
+
+  @spec ex1(list(charlist())) :: non_neg_integer()
+  def ex1(lines) do
+    lines |> Enum.join() |> get_mul()
+  end
+
+  @spec ex1(list(charlist())) :: non_neg_integer()
+  def ex2(lines) do
+    lines |> Enum.join() |> remove_disable_parts() |> get_mul()
+  end
+end
