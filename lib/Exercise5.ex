45113f7ca0a9211b5b00abcdc2c4077b596b90df
Alexandre C <beejayvolts@gmail.com>
2024-12-05 14:18:33 +0100

    feat(ex05)

---
+++ lib/Exercise5.ex
@@ -0,0 +1,60 @@
+defmodule Exercise5 do
+  @spec extract_integers_from_char(list(String.t()), String.t()) :: list(list(integer()))
+  defp extract_integers_from_char(lines, char) do
+    lines |> Enum.filter(fn str -> str |> String.contains?(char) end)
+    |> Enum.map(fn str -> str |> String.split(char, trim: true) |> Enum.map(&String.to_integer/1) end)
+  end
+
+  @spec has_matching_pair?(list({integer(), integer()}), integer(), list(integer())) :: boolean()
+  defp has_matching_pair?(page_order, left_value, instructions) do
+    page_order
+    |> Enum.any?(fn {left, right} -> left === left_value and (instructions |> Enum.member?(right)) end)
+  end
+
+  @spec is_valid_pair?(list({integer(), integer()}), {integer(), integer()}) :: boolean()
+  defp is_valid_pair?(page_order, {left, right}) do
+    page_order |> Enum.any?(fn {a, b} -> (a === left and b === right) end)
+  end
+
+  @spec is_instruction_valid(list({integer(), integer()}), list(integer())) :: boolean()
+  defp is_instruction_valid(page_order, instruction) do
+    if (length(instruction) <= 1) do
+      true
+    else
+      if (!is_valid_pair?(page_order, {hd(instruction), hd(tl(instruction))})) do
+        false
+      else
+        is_instruction_valid(page_order, tl(instruction))
+      end
+    end
+  end
+
+  @spec reorder_invalid_instruction(list({integer(), integer()}), list(integer())) :: list(integer())
+  defp reorder_invalid_instruction(page_order, instruction) do
+      last = instruction |> Enum.find(fn left_value -> !has_matching_pair?(page_order, left_value, instruction) end)
+      if (last === nil) do
+        []
+      else
+        [last] ++ reorder_invalid_instruction(page_order, instruction |> Enum.filter(fn n -> n !== last end))
+      end
+  end
+
+  @spec ex1([String.t()]) :: non_neg_integer()
+  def ex1(lines) do
+    page_order = lines |> extract_integers_from_char("|") |> Enum.map(fn elem -> {hd(elem), hd(tl(elem))} end)
+    lines
+    |> extract_integers_from_char(",")
+    |> Enum.filter(fn instruction -> is_instruction_valid(page_order, instruction) end)
+    |> Enum.reduce(0, fn instruction, acc -> acc + (instruction |> Enum.at(div(length(instruction), 2))) end)
+  end
+
+  @spec ex2([String.t()]) :: non_neg_integer()
+  def ex2(lines) do
+    page_order = lines |> extract_integers_from_char("|") |> Enum.map(fn elem -> {hd(elem), hd(tl(elem))} end)
+    lines
+    |> extract_integers_from_char(",")
+    |> Enum.filter(fn instruction -> !is_instruction_valid(page_order, instruction) end)
+    |> Enum.map(fn instruction -> reorder_invalid_instruction(page_order, instruction) end)
+    |> Enum.reduce(0, fn instruction, acc -> acc + (instruction |> Enum.at(div(length(instruction), 2))) end)
+  end
+end
--- lib/advent_of_code2024.ex
+++ lib/advent_of_code2024.ex
@@ -9,7 +9,8 @@ defmodule AdventOfCode2024 do
   end
 
   def run_function({func, index}) do
-    AdventOfCode2024.read_file("assets/#{div(index, 2)}.txt") |> func.() |> IO.puts()
+    output = AdventOfCode2024.read_file("assets/#{div(index, 2)}.txt") |> func.()
+    IO.puts("Exercise #{div(index, 2)}-#{rem(index, 2) + 1}: #{output}")
   end
 
   @spec main() :: :ok
@@ -19,7 +20,8 @@ defmodule AdventOfCode2024 do
       &Exercise1.ex1/1, &Exercise1.ex2/1,
       &Exercise2.ex1/1, &Exercise2.ex2/1,
       &Exercise3.ex1/1, &Exercise3.ex2/1,
-      &Exercise4.ex1/1, &Exercise4.ex2/1
+      &Exercise4.ex1/1, &Exercise4.ex2/1,
+      &Exercise5.ex1/1, &Exercise5.ex2/1
     ]
     functions |> Enum.with_index() |> Enum.each(&AdventOfCode2024.run_function/1)
   end
