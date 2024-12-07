1999bd2cd9229b4697d5eb60adf4d22f50ee83d3
Alexandre C <beejayvolts@gmail.com>
2024-12-07 10:49:49 +0100

    feat(ex07)

--- lib/advent_of_code2024.ex
+++ lib/advent_of_code2024.ex
@@ -22,7 +22,8 @@ defmodule AdventOfCode2024 do
       &Exercise3.ex1/1, &Exercise3.ex2/1,
       &Exercise4.ex1/1, &Exercise4.ex2/1,
       &Exercise5.ex1/1, &Exercise5.ex2/1,
-      &Exercise6.ex1/1, &Exercise6.ex2/1
+      &Exercise6.ex1/1, &Exercise6.ex2/1,
+      &Exercise7.ex1/1, &Exercise7.ex2/1
     ]
     functions |> Enum.with_index() |> Enum.each(&AdventOfCode2024.run_function/1)
   end
